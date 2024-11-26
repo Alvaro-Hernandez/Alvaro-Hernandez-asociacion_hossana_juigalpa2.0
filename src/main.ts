acfd369e223a1a60f9f6e29a984830fc9dea1330
Alvaro-Hernandez <alvaropineda606@gmail.com>
2024-11-25 22:43:30 -0600

    :lipstick:Add firebase initialization globally

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { mergeApplicationConfig } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    ...appConfig.providers,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+  ],
+});
