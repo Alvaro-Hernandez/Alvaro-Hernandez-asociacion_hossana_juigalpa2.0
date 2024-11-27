78aaaf6185c92f53942d9e30d59491342e603c35
Alvaro-Hernandez <alvaropineda606@gmail.com>
2024-11-27 12:41:48 -0600

    :sparkles:Add leader access component

---
+++ src/app/pages/accessleaders/accessleaders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accessleaders',
+  standalone: true,
+  imports: [],
+  templateUrl: './accessleaders.component.html',
+  styleUrl: './accessleaders.component.css'
+})
+export class AccessleadersComponent {
+
+}
