e00d323da45a92a79fa1a6425c264592984e7e93
Alvaro-Hernandez <alvaropineda606@gmail.com>
2024-11-27 12:43:24 -0600

    :sparkles:Add friendship circle component

---
+++ src/app/pages/friendshipcircle/friendshipcircle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friendshipcircle',
+  standalone: true,
+  imports: [],
+  templateUrl: './friendshipcircle.component.html',
+  styleUrl: './friendshipcircle.component.css'
+})
+export class FriendshipcircleComponent {
+
+}
