2337e092cbf4858d7778a505b9b973272bc6b14d
diedrizon <diedrinzonfargas@gmail.com>
2024-12-05 11:09:37 -0600

    :lipstick: Se creo el boton de descrubir mas imagenes, se le cambio el borde a la galeria.

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -17,7 +17,21 @@ export class GalleryComponent {
     'assets/images/6.jpg',
     'assets/images/7.jpg',
     'assets/images/8.jpg',
-    'assets/images/9.jpg'
+    'assets/images/9.jpg',
+
+
+    
+    'assets/images/10.jpg',
+    'assets/images/11.jpg',
+    'assets/images/12.jpg',
+    'assets/images/13.jpg',
+    'assets/images/14.jpg',
+    'assets/images/15.jpg',
+    'assets/images/16.jpg',
+    'assets/images/17.jpg',
+    'assets/images/11.jpg',
+    'assets/images/12.jpg',
+    'assets/images/10.jpg'
   ];
 
   isModalOpen: boolean = false;
@@ -33,21 +47,27 @@ export class GalleryComponent {
     this.isModalOpen = true;
   }
 
-  // Esto cierra el modal
+  // Abre el modal mostrando la primera imagen
+  openAllImages(): void {
+    this.currentIndex = 0;
+    this.isModalOpen = true;
+  }
+
+  // Cierra el modal
   closeModal(): void {
     this.isModalOpen = false;
   }
 
-  // Muesta la siguiente imagen
+  // Muestra la siguiente imagen
   nextImage(): void {
     if (this.currentIndex < this.images.length - 1) {
       this.currentIndex++;
     } else {
-      this.currentIndex = 0; 
+      this.currentIndex = 0;
     }
   }
 
-  // Esto muestra la imagen anterior
+  // Muestra la imagen anterior
   prevImage(): void {
     if (this.currentIndex > 0) {
       this.currentIndex--;
